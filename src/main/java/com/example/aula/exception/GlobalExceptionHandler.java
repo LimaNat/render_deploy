171eb243a76ae00de3b37f1cd3f389aa3536735a
LimaNat <160279258+LimaNat@users.noreply.github.com>
2025-05-27 11:35:30 -0300

    ok

--- src/main/java/com/example/aula/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/aula/exception/GlobalExceptionHandler.java
@@ -34,7 +34,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, Object>> handlerMethodArgumentNotValidException(MethodArgumentNotValidException erro) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of("mensagem", Objects.requireNonNull(erro.getFieldErrors().getFirst().getDefaultMessage())));
+                .body(Map.of("mensagem", erro.getFieldErrors().getFirst().getDefaultMessage()));
     }
 
     @ExceptionHandler(NomeJaCadastradoException.class)
